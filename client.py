285f612da3216bc8a61f05ab771eb1ec53d45044
lidiaenbla <lidiaeb1993@gmail.com>
2016-11-14 10:59:06 +0100

    empezando

--- client.py
+++ client.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Programa cliente que abre un socket a un servidor
+Programa cliente UDP que abre un socket a un servidor
 """
 
 import socket
+import sys
 
-# Cliente UDP simple.
+SIP = ""
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+try:
+    METHOD = sys.argv[1]
+    SIP_METHOD = sys.argv[2]
+except IndexError:
+    sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
+if METHOD == "INVITE":
+    PORT = SIP_METHOD.split(":", -1)[1]
+    SIP = SIP_METHOD.split(":", -1)[0]
+    SERVER = SIP.split("@")[1]
+    SIP = "sip:" + SIP + " SIP/2.0\r\n"
+    LINE = "INVITE " + SIP + "\r\n"
+elif METHOD == "BYE":
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.connect((SERVER, PORT))
+    print("Enviando:", LINE)
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
+    print('Recibido -- ', data.decode('utf-8'))
 
-# Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
-my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
-
-print("Enviando: " + LINE)
-my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-data = my_socket.recv(1024)
-
-print('Recibido -- ', data.decode('utf-8'))
-print("Terminando socket...")
-
-# Cerramos todo
-my_socket.close()
-print("Fin.")
+print("Socket terminado.")
