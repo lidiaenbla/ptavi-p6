8ff00cfb4be594e0a5d5cc0f557faf1c03a2e916
lidiaenbla <lidiaeb1993@gmail.com>
2016-11-14 17:31:52 +0100

    server con sus metodos

--- server.py
+++ server.py
@@ -5,27 +5,46 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import socketserver
+import time
+import json
+import sys
 
+try:
+    IP = sys.argv[1]
+    PORT = int(sys.argv[2])
+    FILE = sys.argv[3]
+except IndexError:
+    sys.exit("Usage: python3 server.py IP port audio_file")
 
-class EchoHandler(socketserver.DatagramRequestHandler):
+class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     """
     Echo server class
     """
-
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write(b"Hemos recibido tu peticion")
-        while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print("El cliente nos manda " + line.decode('utf-8'))
-
-            # Si no hay más líneas salimos del bucle infinito
-            if not line:
-                break
+        """
+        Manejador
+        """
+        line = self.rfile.read()
+        print("El cliente nos manda ", line.decode('utf-8'))
+        linea = line.decode('utf-8').split()
+        print("linea",linea)
+        if linea[0] == "INVITE":
+            if linea[1].split("@"):
+                self.wfile.write(b"SIP/2.0 100 Trying\r\n\r\n SIP/2.0 180 Ring\r\n\r\n SIP/2.0 200 OK\r\n\r\n")
+            else:
+                self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
+        elif linea[0] == "BYE":
+            self.wfile.write(b"200 OK\r\n\r\n")
+        elif linea[0] == "ACK":
+            self.wfile.write(b"enviamos la cancion\r\n\r\n")
+        else:
+            self.wfile.write(b"SIP/2.0 405 Method Not Allowed\r\n\r\n")
 
 if __name__ == "__main__":
-    # Creamos servidor de eco y escuchamos
-    serv = socketserver.UDPServer(('', 6001), EchoHandler)
-    print("Lanzando servidor UDP de eco...")
-    serv.serve_forever()
+
+    serv = socketserver.UDPServer((('', PORT)), SIPRegisterHandler)
+    print("Listening...")
+    try:
+        serv.serve_forever()
+    except KeyboardInterrupt:
+        print("Finalizado servidor")
