26d648f8e9ed6dde1d356a55b80f8059dadce6b3
lidiaenbla <lidiaeb1993@gmail.com>
2016-11-21 10:57:26 +0100

    terminado

--- client.py
+++ client.py
@@ -16,12 +16,12 @@ except IndexError:
     sys.exit("Usage: python3 client.py method receiver@IP:SIPport")
 
 PORT = int(SIP_METHOD.split(":", -1)[1])
-SIP = "sip:" + SIP_METHOD.split(":", -1)[0] + " SIP/2.0\r\n\r\n"
+SIP = "sip:" + SIP_METHOD.split(":", -1)[0] + " SIP/2.0\r\n"
 
 if METHOD == "INVITE":
-    LINE = "INVITE " + SIP + "\r\n"
+    LINE = "INVITE " + SIP
 elif METHOD == "BYE":
-    LINE = "BYE " + SIP + "\r\n"
+    LINE = "BYE " + SIP 
 
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect(('127.0.0.1', PORT))
@@ -30,7 +30,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     print(data.decode('utf-8'))
     data = data.decode('utf-8').split()
     if data[1] == "100" and data[4] == "180" and data[7] == "200":
-        LINE = "ACK " + SIP + "\r\n"
+        LINE = "ACK " + SIP
         my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 
 print("Socket terminado.")
